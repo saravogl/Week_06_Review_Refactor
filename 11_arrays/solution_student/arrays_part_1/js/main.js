e95cd736ef0bf8a6c9e751437195daa7f4751b46
poopsplat <matt.m.soria@gmail.com>
2016-01-21 00:55:01 -0600

    Pushing up Lesson 12 files

--- 11_arrays/solution_student/arrays_part_1/js/main.js
+++ 11_arrays/solution_student/arrays_part_1/js/main.js
@@ -4,16 +4,16 @@
 // -------------------------------------------------------------------------------
 
 // 1. Declare a variable called "groceries" and assign it an array of strings: "milk", "eggs", "salmon", "asparagus"
-
+var groceries = ["milk", "eggs", "salmon", "asparagus"];
 
 // 2. Add a fifth item to the array: "apples"
-
+groceries[4] = "apples";
 
 // 3. Replace the second item in the array. The new value should be "brussel sprouts"
-
+groceries[1] = "brussel sprouts";
 
 // 4. Add a sixth item to the array: "oranges"
-
+groceries[5] = "oranges";
 
 
 
